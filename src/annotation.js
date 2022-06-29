09389922f38fff2fbfcb1b6c72e35f045c9e38b8
maziyank <maziyank@gmail.com>
2022-06-30 09:26:16 +1000

    refactor unit test

--- src/annotation.js
+++ src/annotation.js
@@ -1,16 +1,16 @@
 function annotate(text) {
 
     const normalize = (txt) => {
-        txt = txt.replace("see, generally,", "see")
-        txt = txt.replace("see,", "see")
-        txt = txt.replace(/\(\S+\)/gm, x => x.replace(/\(\S+\)/gm, x => x.replace(/\(|\)/g, '-')))
+        txt = txt.replace("see, generally,", "see");
+        txt = txt.replace("see,", "see");
+        txt = txt.replace(/\(\S+\)/gm, x => x.replace(/\(\S+\)/gm, x => x.replace(/\(|\)/g, '-')));
         return txt;
     }
 
     const denormalize = (txt) => {
         if (!txt) return
-        txt = txt.replace(/\-\S+\-/gm, x => `(${x.slice(1, x.length - 1)})`)
-        txt = txt.replace(new RegExp(prefix), "")
+        txt = txt.replace(/\-\S+\-/gm, x => `(${x.slice(1, x.length - 1)})`);
+        txt = txt.replace(new RegExp(prefix), "");
         return txt;
     }
     // normalize text
@@ -19,22 +19,21 @@ function annotate(text) {
     // find v position
     v_indices = Array.from(text.matchAll(/\sv\s/gm), match => match.index);
 
-    const prefix = "(((^|\.|\:| )(see)|(but)|(in)|(applied)|([^A-Z]+\sof)|(cites)|(on)|(by)|(at)|(with)|(to)) )|\\("
-    const prefix_regex = new RegExp(prefix, "gm")
+    const prefix = "(((^|\.|\:| )(see)|(but)|(in)|(applied)|(^[A-Z]+\sof)|(cites)|(on)|(by)|(at)|(with)|(to)) )|\\(";
+    const prefix_regex = new RegExp(prefix, "gm");
     prefix_indices = Array.from(text.matchAll(prefix_regex), match => match);
 
     const start = v_indices.map(v_index => {
-        const cadidate = prefix_indices.filter(item => item.index < v_index)
-        return cadidate[cadidate.length - 1].index
+        const candidate = prefix_indices.filter(item => item.index < v_index);
+        return candidate[candidate.length - 1].index;
     })
 
-    const end = /(.*(|\[|\\(\d{4}\]|\\|)).*([A-Z]\w+)*\s(\d+\/\d+|\d+))|(.*[A-Z]+\/\d+\/\d+\/[A-Z]+)/g
-    const citations = start.map((s, i) => denormalize(Array.from(text.slice(s, v_indices[i + 1]).matchAll(end), match => match[0])[0]))
-
+    const end = /(.*(|\[|\\(\d{4}\]|\\|)).*([A-Z]\w+)*\s(\d+\/\d+|\d+))|(.*[A-Z]+\/\d+\/\d+\/[A-Z]+)/g;
+    const citations = start.map((s, i) => denormalize(Array.from(text.slice(s, v_indices[i + 1]).matchAll(end), match => match[0])[0]));
+ 
     // denormalize
-    return citations
+    return citations;
 }
 
-// console.log(annotate('Appeal Tribunal in Nicholls v Rockwell Automation Ltd EAT/0540/11/SM'))
-console.log(annotate("the tribunal see Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06"))
+console.log(annotate("the tribunal see Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06."))
 module.exports = annotate;
--- test/annotation.test.js
+++ test/annotation.test.js
@@ -1,40 +1,44 @@
 const annotate = require('./../src/annotation');
 
-test('W v W [2011] EWCA Civ 703', () => {
-    expect(annotate('Court of Appeal in W v W [2011] EWCA Civ 703 which suggest that the jurisdictional basis on which')).toStrictEqual(['W v W [2011] EWCA Civ 703']);
-});
+scenarios = [
+    {
+        text: 'Court of Appeal in W v W [2011] EWCA Civ 703 which suggest that the jurisdictional basis on which',
+        expected: 'W v W [2011] EWCA Civ 703'
+    },
+    {
+        text: 'That text cites Philips v Pearce [1996] 2 FLR 230, where Johnson J held that he was unable to make a maintenance order',
+        expected: 'Philips v Pearce [1996] 2 FLR 230'
+    },
+    {
+        text: '(see Jones v University of Lincolnshire and Humberside [2000] 1 WLR 1988 at [43] and the authorities referred to in that paragraph)',
+        expected: 'Jones v University of Lincolnshire and Humberside [2000] 1 WLR 1988'
+    },
+    {
+        text: 'see BB v Secretary of State for Work and Pensions (PIP) [2017] UKUT (AAC) 596',
+        expected: 'BB v Secretary of State for Work and Pensions (PIP) [2017] UKUT (AAC) 596'
+    },
+    {
+        text: 'was emphasised in Mugford v Midland Bank plc 1997 ICR 399, EAT',
+        expected: 'Mugford v Midland Bank plc 1997 ICR 399'
+    },
+    {
+        text: 'Appeal Tribunal in Nicholls v Rockwell Automation Ltd EAT/0540/11/SM.',
+        expected: 'Nicholls v Rockwell Automation Ltd EAT/0540/11/SM'
+    },
+    {
+        text: 'compensation awarded (pursuant to Polkey v AE Dayton Services Ltd [1988] ICR 142). Those judgments..',
+        expected: 'Polkey v AE Dayton Services Ltd [1988] ICR 142'
+    },
+    {
+        text: 'the tribunal see Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06 in the bank',
+        expected: 'Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06'
+    }
+]
 
-expect1 = "Philips v Pearce [1996] 2 FLR 230";
-test(expect1, () => {
-    expect(annotate('That text cites Philips v Pearce [1996] 2 FLR 230, where Johnson J held that he was unable to make a maintenance orde')).toStrictEqual([expect1]);
-});
 
-expect2 = "Jones v University of Lincolnshire and Humberside [2000] 1 WLR 1988";
-test(expect2, () => {
-    expect(annotate('(see Jones v University of Lincolnshire and Humberside [2000] 1 WLR 1988 at [43] and the authorities referred to in that paragraph)')).toStrictEqual([expect2]);
-});
 
-expect3 = "Home Office v Tariq [2011] UKSC 35";
-test(expect3, () => {
-    expect(annotate('respondent (Home Office v Tariq [2011] UKSC 35 at [102]-[105])')).toStrictEqual([expect3]);
-});
-
-expect4 = "BB v Secretary of State for Work and Pensions (PIP) [2017] UKUT (AAC) 596";
-test(expect4, () => {
-    expect(annotate('see BB v Secretary of State for Work and Pensions (PIP) [2017] UKUT (AAC) 596')).toStrictEqual([expect4]);
-});
-
-expect5 = "Mugford v Midland Bank plc 1997 ICR 399";
-test(expect5, () => {
-    expect(annotate('was emphasised in Mugford v Midland Bank plc 1997 ICR 399, EAT')).toStrictEqual([expect5]);
-});
-
-expect6 = 'Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06';
-test(expect6, () => {
-    expect(annotate('the tribunal see Manchester College of Arts and Technology (MANCAT) v Mr G Smith [2007] UKEAT 0460/06')).toStrictEqual([expect6]);
-});
-
-expect7 = 'Nicholls v Rockwell Automation Ltd EAT/0540/11/SM';
-test(expect7, () => {
-    expect(annotate('Appeal Tribunal in Nicholls v Rockwell Automation Ltd EAT/0540/11/SM.')).toStrictEqual([expect7]);
-});
+scenarios.forEach(({ text, expected }) => {
+    test(expected, () => {
+        expect(annotate(text)).toStrictEqual([expected]);
+    });
+})
