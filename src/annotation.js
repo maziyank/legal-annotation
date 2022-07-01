dbf6e5fb26a0f32dbfecaace0cbdc9e1cc1d7c9c
maziyank <maziyank@gmail.com>
2022-07-01 13:34:25 +1000

    add rule for new test case

--- src/annotation.js
+++ src/annotation.js
@@ -4,13 +4,15 @@ function annotate(text) {
         txt = txt.replace("see, generally,", "see");
         txt = txt.replace("see,", "see");
         txt = txt.replace(/\:\s*/ , '\n');
-        txt = txt.replace(/\(\S+\)/gm, x => x.replace(/\(\S+\)/gm, x => x.replace(/\(|\)/g, '-')));
+        txt = txt.replace(/(\(\S+\))(?=.*\sv\.?\s)/gm,  x => x.replace(/\(|\)/g, '-'));
+        txt = txt.replace(/(?<=[A-Z]\w+)(\sof\s)(?=[A-Z])/gm,  x => x.replace(' of ', ' %OF% '));
         return txt;
     }
 
     const denormalize = (txt) => {
         if (!txt) return
         txt = txt.replace(/\-\S+\-/gm, x => `(${x.slice(1, x.length - 1)})`);
+        txt = txt.replace(' %OF% ', ' of ');
         txt = txt.replace(new RegExp(prefix), "");
         return txt;
     }
@@ -18,12 +20,12 @@ function annotate(text) {
     text = normalize(text);
 
     // find v position
-    v_indices = Array.from(text.matchAll(/\sv\s/gm), match => match.index);
- 
+    v_indices = Array.from(text.matchAll(/\sv\.?\s/gm), match => match.index);
+
     if (!v_indices)
         return false
 
-    const prefix = "((\\. +|\:|\\(| |^)((see)|(but)|(in)|(applied)|(accord)|(^[A-Z]+\sof)|(cites)|(on)|(by)|(at)|(with)|(to)) )|\\(|^|\\n";
+    const prefix = "((\\. +|\:|\\(| |^)((see)|(but)|(in)|(of)|(applied)|(accord)|(cites)|(on)|(by)|(at)|(with)|(to)) )|\\(|^|\\n";
     const prefix_regex = new RegExp(prefix, "gm");
     prefix_indices = Array.from(text.matchAll(prefix_regex), match => match);
 
@@ -36,11 +38,13 @@ function annotate(text) {
     })
      if (!start) return false
 
-    const end = /(.*(|\[|\\(\d{4}\]|\\|)).*([A-Z]\w+)*\s(\d+\/\d+|\d+))|(.*[A-Z]+\/\d+\/\d+\/[A-Z]+)/g;
+     console.log(prefix_indices.map(m=>m.index), start)
+
+    const end = /(.*(|\[|\\(\d{4}\]|\\|)).*([A-Z]\w+)*\s(\d+\/\d+|\d+))|(.*[A-Z]+\/\d+\/\d+\/[A-Z]+)|(.*(\(\w+\/\d+\/\d+\)))/g;
     const citations = start.map((s, i) => denormalize(Array.from(text.slice(s, v_indices[i + 1]).matchAll(end), match => match[0])[0]));
 
     // denormalize
     return citations;
 }
 
- module.exports = annotate;
+module.exports = annotate;
