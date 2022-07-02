227df8846283bbfa0d959a81bf52d1578134895b
maziyank <maziyank@gmail.com>
2022-07-02 11:29:02 +1000

    add new case

--- src/annotation.js
+++ src/annotation.js
@@ -2,10 +2,28 @@ const prefix = "((\\. +|\:|\\(| |^)((see)|(but)|(in)|(of)|(applied)|(accord)|(ci
 const prefix_regex = new RegExp(prefix, "gm");
 const end = /(.*(|\[|\\(\d{4}\]|\\|)).*([A-Z]\w+)*\s(\d+\/\d+|\d+))|(.*[A-Z]+\/\d+\/\d+\/[A-Z]+)|(.*(\(\w+\/\d+\/\d+\)))/g;
 
+const indexOfGroup = (match, n) => {
+    var ix= match.index;
+    for (var i= 1; i<n; i++)
+        ix+= match[i].length;
+    return ix;
+}
+
 const normalize = (txt) => {
     txt = txt.replace("see, generally,", "see");
     txt = txt.replace("see,", "see");
-    txt = txt.replace(/\:\s*/, '\n');
+    txt = txt.replace(/,?\sand\s/g, " and ");
+    txt = txt.replace(/\:\s*/, '\n'); 
+
+    // split if `and` found 
+    const and_ = /(\sv\s.*((|\[|\\(\d{4}\]|\\|))([A-Z]\w+)*\s(\d+\/\d+|\d+))|([A-Z]+\/\d+\/\d+\/[A-Z]+)|((\(\w+\/\d+\/\d+\))))(\s+and\s+)/g
+    match_and_ = Array.from(txt.matchAll(and_))[0]
+    if (match_and_){
+        index_and_ = match_and_.index + match_and_[0].length    
+        txt = txt.substring(0, index_and_) + '\n' + txt.substring(index_and_);
+    }
+    
+    // preserve
     txt = txt.replace(/(\(\S+\))(?=.*\sv\.?\s)/gm, x => x.replace(/\(|\)/g, '-'));
     txt = txt.replace(/(?<=[A-Z]\w+)(\sof\s)(?=[A-Z])/gm, x => x.replace(' of ', ' %OF% '));
     return txt;
@@ -15,7 +33,7 @@ const denormalize = (txt) => {
     if (!txt) return
     txt = txt.replace(/\-\S+\-/gm, x => `(${x.slice(1, x.length - 1)})`);
     txt = txt.replace(' %OF% ', ' of ');
-    // txt = txt.replace(new RegExp(prefix), "");
+    txt = txt.trim();
     return txt;
 }
 
@@ -60,5 +78,5 @@ function annotate(text) {
     return [...new Set(citations)];
 }
 
-// console.log(annotate("We need only refer to the well-known authorities of Meek v City of Birmingham DC [1987] IRLR 250 and"))
+// console.log(annotate("We need only refer to the well-known authorities of Meek v City of Birmingham DC [1987] IRLR 250 and English v Emery Reimbold and Strick [2003] IRLR 710 for"))
 module.exports = annotate;
