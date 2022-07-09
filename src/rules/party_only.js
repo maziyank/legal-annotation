b2d6c61917224a7a0de57ef96cbecf1839194d27
maziyank <maziyank@gmail.com>
2022-07-09 11:34:34 +1000

    add documentation

--- src/rules/party_only.js
+++ src/rules/party_only.js
@@ -1,6 +1,13 @@
 const RGX = require("./../utils/_general_regex");
 const { denormalize } = require("./../utils/_normalize");
 
+/**
+* @description  Given text as input, capture citation which has party with date  (i.e. Henderson vs Ronaldo (23 July 2020))
+* @function cit_party_date
+* @param {String} text input raw text
+* @return {[String]}    list of captured citation
+*/
+
 const cit_party_date = (text) => {
     const RGX_PARTY_WITH_DATE = new RegExp(`${RGX.PARTY_NAME.source}(\\s+[\\–\\-]?v\\.?)${RGX.PARTY_NAME.source}\\s+\\\(${RGX.DATE_DDMMMMYYYY.source}\\\)`, 'gm');
     const matched = Array.from(text.matchAll(RGX_PARTY_WITH_DATE));
@@ -8,12 +15,26 @@ const cit_party_date = (text) => {
     return result
 }
 
+/**
+* @description Given text as input, capture citation which has party and unreported date (i.e. Henderson vs Ronaldo (23 July 2020, unreported))
+* @function cit_party_unreported
+* @param {String} text 
+* @return {[String]}    list of captured citation
+*/
+
 const cit_party_unreported = (text) => {
     const RGX_PARTY_UNREPORTED = new RegExp(`${RGX.PARTY_NAME.source}(\\s+[\\–\\-]?v\\.?)${RGX.PARTY_NAME.source}\\s+\\\(${RGX.DATE_UNREPORTED.source}\\\)`, 'gm');
     const matched = Array.from(text.matchAll(RGX_PARTY_UNREPORTED));
     const result = matched.map(m => denormalize(m[0]));
     return result
 }
+
+/**
+* @description Given text as input, capture citation which has only party (i.e. Henderson vs Ronaldo)
+* @function cit_party_only
+* @param {String} text 
+* @return {[String]}    list of captured citation
+*/
 
 const cit_party_only = (text) => {
     const RGX_TEST = new RegExp(`${RGX.PARTY_NAME.source}(\\s+[\\–\\-]?v\\.?)${RGX.PARTY_NAME.source}(.{0,30})`, 'gm');
@@ -30,3 +51,6 @@ const cit_party_only = (text) => {
 }
 
 module.exports = { cit_party_date, cit_party_unreported, cit_party_only }
+
+cit_party_only()
+
