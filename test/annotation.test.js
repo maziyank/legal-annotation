83a19d9b81eac0af8383bdbb97af9e7692c859e6
maziyank <maziyank@gmail.com>
2022-08-09 09:00:43 +1000

    fix test suite

--- test/annotation.test.js
+++ test/annotation.test.js
@@ -2,7 +2,8 @@ const annotate = require('../src');
 const test_case = require('./../dataset/dataset.json');
 
 test_case.scenarios.forEach(({ text, expected }) => {
-    test(text + (expected.length>0 ? ' => ': '' ) +  expected.join('; '), () => {
-        expect(annotate(text).sort()).toEqual(expected.sort());
+    test(text + (expected.length>0 ? ' => ': '' ) +  expected.join('; '), async () => {
+        preds = await annotate(text);
+        expect(preds.sort()).toEqual(expected.sort());
     });
 })
